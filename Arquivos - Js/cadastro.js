829054c995f3cb0d19b9370cccae9e8acaeba707
brunagiammelaro <138893476+brunagiammelaro@users.noreply.github.com>
2023-11-22 20:43:56 -0300

    Css - Cadastro

--- Arquivos - Js/cadastro.js
+++ Arquivos - Js/cadastro.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var radioAdotarElement = document.getElementById("iadotar");
-  var radioDoarElement = document.getElementById("idoar");
-  var campoAdotarElement = document.getElementById("campo-adotar");
-  var campoDoarElement = document.getElementById("campo-doar");
-
-
-  radioAdotarElement.addEventListener("change", function() {
+    var radioAdotarElement = document.getElementById("iadotar");
+    var radioDoarElement = document.getElementById("idoar");
+    var campoAdotarElement = document.getElementById("campo-adotar");
+    var campoDoarElement = document.getElementById("campo-doar");
   
-      campoAdotarElement.style.display = "block";
-      campoDoarElement.style.display = "none";
+    // Adiciona um ouvinte de evento para cada botão de rádio
+    radioAdotarElement.addEventListener("change", function() {
+        // Mostra o campo de adoção e oculta o campo de doação
+        campoAdotarElement.style.display = "block";
+        campoDoarElement.style.display = "none";
+    });
+  
+    radioDoarElement.addEventListener("change", function() {
+        // Mostra o campo de doação e oculta o campo de adoção
+        campoDoarElement.style.display = "block";
+        campoAdotarElement.style.display = "none";
+    });
   });
-
-  radioDoarElement.addEventListener("change", function() {
-    
-      campoDoarElement.style.display = "block";
-      campoAdotarElement.style.display = "none";
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    var inputFoto = document.getElementById("foto");
+    var nomeArquivoSpan = document.getElementById("nome-arquivo");
+  
+    inputFoto.addEventListener("change", function () {
+        if (inputFoto.files.length > 0) {
+            nomeArquivoSpan.textContent = " " + inputFoto.files[0].name;
+        } else {
+            nomeArquivoSpan.textContent = ""; // Limpa o texto se nenhum arquivo estiver selecionado
+        }
+    });
   });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var inputFoto = document.getElementById("foto");
-  var nomeArquivoSpan = document.getElementById("nome-arquivo");
-
-  inputFoto.addEventListener("change", function () {
-      if (inputFoto.files.length > 0) {
-          nomeArquivoSpan.textContent = " " + inputFoto.files[0].name;
-      } else {
-          nomeArquivoSpan.textContent = ""; 
-      }
-  });
-});
